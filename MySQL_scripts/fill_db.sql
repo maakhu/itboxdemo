0fad529b2c065386ee3d1b12accdca05354409e1
maakhu <egri@me.com>
2023-06-13 16:31:08 +0200

    fill db with random data

---
+++ MySQL_scripts/fill_db.sql
@@ -0,0 +1,22 @@
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('01.01.2013', '1', 'telep', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('01.01.2013', '1', 'berlet', '1', 'db');
+
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-03-31', '2', 'telep', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-03-31', '2', 'berlet', '1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-04-01', '2', 'berlet', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-04-01', '2', 'telep', '1', 'db');
+
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-04-02', '3', 'telep', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-04-02', '3', 'berlet', '1', 'db');
+
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-12-30', '4', 'telep', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-12-30', '4', 'berlet', '1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-12-31', '3', 'berlet', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-12-31', '3', 'telep', '1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-12-31', '5', 'telep', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2013-12-31', '5', 'berlet', '1', 'db');
+
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2014-12-31', '6', 'telep', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2014-12-31', '6', 'berlet', '1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2014-12-31', '5', 'berlet', '-1', 'db');
+INSERT INTO `itbox`.`cikktetel` (`datum`, `cikkszam`, `raktarkod`, `mennyiseg`, `mertekegyseg`) VALUES ('2014-12-31', '5', 'telep', '1', 'db');
