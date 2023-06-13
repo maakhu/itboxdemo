e335cc67ef96be297e6e709c4d057dd1616b1a7b
maakhu <egri@me.com>
2023-06-13 16:54:20 +0200

    add first get db query

--- db.ts
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2";
-
-let pool = mysql.createPool({   //creates a pool of connections
-  host: "127.0.01", // localhost
-  user: "root",
-  password: "password",
-  database: "itbox",
-}).promise(); //promise API version to avoid callback hell
