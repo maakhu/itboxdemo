67987e1902bbc3c79183fc2b65cde08ab8e63020
maakhu <egri@me.com>
2023-06-13 17:21:28 +0200

    add getItems and getItem logic to db file

--- db.js
+++ db.js
@@ -12,12 +12,12 @@ let pool = mysql
   })
   .promise(); //promise API version to avoid callback hell
 
-async function getItems() {
+export async function getItems() {
   const [rows] = await pool.query("SELECT * FROM cikktetel"); //takes the first element of the array
   return rows;
 }
 
-async function getItem(id) {
+export async function getItem(id) {
   const [rows] = await pool.query(`
     SELECT * FROM cikktetel 
     WHERE tetelszam=?`, [id]);
