ac9e4c43bca30febe2717175eea981f5cb2ad76a
maakhu <egri@me.com>
2023-06-15 00:01:09 +0200

    add fetch correction

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import { getItems, getItem, getItemsInRentByDate } from "./db.js";
 const app = express();
@@ -17,14 +18,23 @@ app.get("/cikktetelek/:id", async (req, res) => {
 
 app.get("/berbeadottak/:date", async (req, res) => {
   const date = req.params.date;
-  console.log("date " + date)
+  console.log("date " + date);
   const inRent = await getItemsInRentByDate(date);
   res.send(inRent);
 });
 
+//error handling should be implemented detailedly, but we don't need it now based on the task, basic error handling below
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
+// app.use(cors({ "Access-Control-Allow-Origin": "*" }));
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.listen(8080, () => console.log("Server is running on port 8080"));
